642180ba4fad1c713a0c57cb5289c509f5a6416d
Xiangyu Zhang <zhangxy@live.com>
2016-01-30 09:29:13 +0800

    assignment1的准备代码全部添加。

--- assignment1.py
+++ assignment1.py
@@ -39,7 +39,7 @@ def extract(filename):
   if is_extract:
       tar = tarfile.open(filename)
       root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
-      print('Extracting data for %s. This may take a while. Please wait.' % root)
+      print 'Extracting data for %s. This may take a while. Please wait.' % root
       sys.stdout.flush()
       tar.extractall()
       tar.close()
@@ -51,8 +51,80 @@ def extract(filename):
     raise Exception(
       'Expected %d folders, one per class. Found %d instead.' % (
         num_classes, len(data_folders)))
-  print(data_folders)
+  print data_folders
   return data_folders
   
 train_folders = extract(train_filename)
 test_folders = extract(test_filename)
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load(data_folders, min_num_images, max_num_images):
+  dataset = np.ndarray(
+    shape=(max_num_images, image_size, image_size), dtype=np.float32)
+  labels = np.ndarray(shape=(max_num_images), dtype=np.int32)
+  label_index = 0
+  image_index = 0
+  for folder in data_folders:
+    print folder
+    for image in os.listdir(folder):
+      if image_index >= max_num_images:
+        raise Exception('More images than expected: %d >= %d' % (
+          image_index, max_num_images))
+      image_file = os.path.join(folder, image)
+      try:
+        image_data = (ndimage.imread(image_file).astype(float) -
+                      pixel_depth / 2) / pixel_depth
+        if image_data.shape != (image_size, image_size):
+          raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+        dataset[image_index, :, :] = image_data
+        labels[image_index] = label_index
+        image_index += 1
+      except IOError as e:
+        print 'Could not read:', image_file, ':', e, '- it\'s ok, skipping.'
+    label_index += 1
+  num_images = image_index
+  dataset = dataset[0:num_images, :, :]
+  labels = labels[0:num_images]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' % (
+        num_images, min_num_images))
+  print 'Full dataset tensor:', dataset.shape
+  print 'Mean:', np.mean(dataset)
+  print 'Standard deviation:', np.std(dataset)
+  print 'Labels:', labels.shape
+  return dataset, labels
+train_dataset, train_labels = load(train_folders, 450000, 550000)
+test_dataset, test_labels = load(test_folders, 18000, 20000)
+
+train_size = 200000
+valid_size = 10000
+
+valid_dataset = train_dataset[:valid_size,:,:]
+valid_labels = train_labels[:valid_size]
+train_dataset = train_dataset[valid_size:valid_size+train_size,:,:]
+train_labels = train_labels[valid_size:valid_size+train_size]
+print 'Training', train_dataset.shape, train_labels.shape
+print 'Validation', valid_dataset.shape, valid_labels.shape
+
+pickle_file = 'notMNIST.pickle'
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print 'Unable to save data to', pickle_file, ':', e
+  raise
+
+statinfo = os.stat(pickle_file)
+print 'Compressed pickle size:', statinfo.st_size
